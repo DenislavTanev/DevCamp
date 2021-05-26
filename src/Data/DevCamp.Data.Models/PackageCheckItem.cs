822e8121ec85ff57b15df442f8cb890facdf6a55
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-05-26 23:45:34 +0300

    Initializing items

---
+++ src/Data/DevCamp.Data.Models/PackageCheckItem.cs
@@ -0,0 +1,15 @@
+﻿namespace DevCamp.Data.Models
+{
+    using DevCamp.Data.Common.Models;
+
+    public class PackageCheckItem : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public bool IsIncluded { get; set; }
+
+        public int PackageId { get; set; }
+
+        public Package Package { get; set; }
+    }
+}
---
+++ src/Data/DevCamp.Data.Models/PackageTextItem.cs
@@ -0,0 +1,15 @@
+﻿namespace DevCamp.Data.Models
+{
+    using DevCamp.Data.Common.Models;
+
+    public class PackageTextItem : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+
+        public int PackageId { get; set; }
+
+        public Package Package { get; set; }
+    }
+}
