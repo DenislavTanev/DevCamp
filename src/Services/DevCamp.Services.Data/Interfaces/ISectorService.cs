b0a335d2a74958a729a47dee79b307a3d3b44dbc
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-06-01 21:31:12 +0300

    Implementing Seeders

---
+++ src/Services/DevCamp.Services.Data/Interfaces/ISectorService.cs
@@ -0,0 +1,9 @@
+﻿namespace DevCamp.Services.Data
+{
+    using System.Collections.Generic;
+
+    public interface ISectorService
+    {
+        IEnumerable<T> GetAll<T>();
+    }
+}
---
+++ src/Services/DevCamp.Services.Data/SectorService.cs
@@ -0,0 +1,30 @@
+﻿namespace DevCamp.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using DevCamp.Data.Common.Repositories;
+    using DevCamp.Data.Models;
+    using DevCamp.Services.Mapping;
+
+    public class SectorService : ISectorService
+    {
+        private readonly IRepository<Sector> sectorsRepository;
+
+        public SectorService(IRepository<Sector> sectorsRepository)
+        {
+            this.sectorsRepository = sectorsRepository;
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            var sectors = this.sectorsRepository
+                .All()
+                .OrderBy(x => x.Name)
+                .To<T>()
+                .ToList();
+
+            return sectors;
+        }
+    }
+}
