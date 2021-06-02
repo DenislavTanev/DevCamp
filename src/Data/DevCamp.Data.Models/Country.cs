0da17822a4785ae1fa482378c105e0fe40120daf
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-06-02 23:40:16 +0300

    Implement Country and Language models.

---
+++ src/Data/DevCamp.Data.Models/Country.cs
@@ -0,0 +1,21 @@
+﻿namespace DevCamp.Data.Models
+{
+    using System.Collections.Generic;
+
+    using DevCamp.Data.Common.Models;
+
+    public class Country : BaseDeletableModel<int>
+    {
+        public Country()
+        {
+            this.Users = new HashSet<ApplicationUser>();
+        }
+
+        public string Name { get; set; }
+
+        // Check
+        public string Flag { get; set; }
+
+        public ICollection<ApplicationUser> Users { get; set; }
+    }
+}
---
+++ src/Data/DevCamp.Data.Models/Language.cs
@@ -0,0 +1,18 @@
+﻿namespace DevCamp.Data.Models
+{
+    using System.Collections.Generic;
+
+    using DevCamp.Data.Common.Models;
+
+    public class Language : BaseDeletableModel<int>
+    {
+        public Language()
+        {
+            this.Users = new HashSet<ApplicationUser>();
+        }
+
+        public string Name { get; set; }
+
+        public ICollection<ApplicationUser> Users { get; set; }
+    }
+}
