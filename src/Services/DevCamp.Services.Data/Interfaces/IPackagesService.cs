64d7bb9dd14edfa54e868e17e014bbc5abd89227
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-06-06 17:15:05 +0300

    Implement package service

--- src/Services/DevCamp.Services.Data/Interfaces/IPackagesService.cs
+++ src/Services/DevCamp.Services.Data/Interfaces/IPackagesService.cs
@@ -1,12 +1,15 @@
 ﻿namespace DevCamp.Services.Data.Interfaces
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public interface IPackagesService
     {
-        Task CreateAsync(string question, string answer, string userId);
+        Task CreateAsync(string name, double price, string packageInfo, string description);
 
         Task<T> GetByIdAsync<T>(int id);
+
+        IEnumerable<T> GetAll<T>(int packageId);
 
         Task EditAsync(int id, string question, string answer, string userId);
     }
