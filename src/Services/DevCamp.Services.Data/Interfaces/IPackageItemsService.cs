aea52c33b27e084354144d2840cc38874e6f3337
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-06-08 04:37:39 +0300

    Add service interfaces

---
+++ src/Services/DevCamp.Services.Data/Interfaces/IPackageItemsService.cs
@@ -0,0 +1,18 @@
+﻿namespace DevCamp.Services.Data.Interfaces
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IPackageItemsService
+    {
+        IEnumerable<T> GetAllByPackage<T>(int packageId);
+
+        Task CreateAsync(int packageId, int itemId, string content, bool isIncluded);
+
+        Task EditAsync(int id, int packageId, int itemId, string content, bool isIncluded);
+
+        Task<T> GetByIdAsync<T>(int id);
+
+        Task DeleteAsync(int id);
+    }
+}
