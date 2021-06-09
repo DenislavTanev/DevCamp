f8d44389bd6d19b23513bf563fd780e2f46518a7
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-06-09 09:28:12 +0300

    Add view model

--- src/Web/DevCamp.Web.ViewModels/Listings/ListingViewModel.cs
+++ src/Web/DevCamp.Web.ViewModels/Listings/ListingViewModel.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DevCamp.Web.ViewModels.Listings
+﻿namespace DevCamp.Web.ViewModels.Listings
 {
-    class ListingViewModel
+    using DevCamp.Data.Models;
+    using DevCamp.Services.Mapping;
+
+    public class ListingViewModel : IMapFrom<Listing>
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ProjectDetails { get; set; }
+
+        public double? StartingPrice { get; set; }
+
+        public string UserId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int SubCategoryId { get; set; }
     }
 }
