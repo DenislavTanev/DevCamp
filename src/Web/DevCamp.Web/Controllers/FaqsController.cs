02debc5bf24c7610849a34481b15206611414e70
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-07-02 20:53:38 +0300

    Implement index action to package controller

--- src/Web/DevCamp.Web/Controllers/FaqsController.cs
+++ src/Web/DevCamp.Web/Controllers/FaqsController.cs
@@ -1,11 +1,30 @@
 ﻿namespace DevCamp.Web.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
-    public class FaqsController
+    public class FaqsController : Controller
     {
+        public FaqsController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        public IActionResult Edit()
+        {
+            return this.View();
+        }
     }
 }
--- src/Web/DevCamp.Web/Controllers/PackagesController.cs
+++ src/Web/DevCamp.Web/Controllers/PackagesController.cs
@@ -1,11 +1,62 @@
 ﻿namespace DevCamp.Web.Controllers
 {
+    using DevCamp.Services.Data.Interfaces;
+    using DevCamp.Web.ViewModels.Packages;
+    using Microsoft.AspNetCore.Mvc;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
-    public class PackagesController
+    public class PackagesController : Controller
     {
+        private readonly IPackagesService packagesService;
+        private readonly IPackageItemsService packageItemsService;
+        private readonly IItemsService itemService;
+
+        public PackagesController(
+            IPackagesService packagesService,
+            IPackageItemsService packageItemsService,
+            IItemsService itemService)
+        {
+            this.packagesService = packagesService;
+            this.packageItemsService = packageItemsService;
+            this.itemService = itemService;
+        }
+
+        public async Task<IActionResult> Index(int packageId)
+        {
+            var package = await this.packagesService.GetByIdAsync<PackageViewModel>(packageId);
+
+            package.ItemContent = this.packageItemsService.GetAllByPackage<PackageItemViewModel>(packageId);
+
+            var items = new List<ItemViewModel>();
+
+            foreach (var item in package.ItemContent)
+            {
+                var currItem = await this.itemService.GetByIdAsync<ItemViewModel>(item.ItemId);
+
+                items.Add(currItem);
+            }
+
+            package.Items = items;
+
+            return this.View(package);
+        }
+
+        public IActionResult ComparePackages()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        public IActionResult Edit()
+        {
+            return this.View();
+        }
     }
 }
